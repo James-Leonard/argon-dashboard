a982057e54d64d7f1c97a9974dbe81b52528a5f9
james-leonard <merrymerry1320@gmail.com>
2022-10-19 23:10:26 +0100

    first deploy

--- core/settings.py
+++ core/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
         'NAME': 'db.sqlite3',
     }
 }
-#db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-#DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
